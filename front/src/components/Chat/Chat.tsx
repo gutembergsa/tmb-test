f828fd2466ba96b9e83d85a008f117917d27c866
gutembergsa <gutemberg.silvasa@outlook.com>
2026-02-18 11:49:05 -0300

    fix: component ts typo

--- front/src/components/Chat/Chat.tsx
+++ front/src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { useChatStore } from '../../utils/zustand/chat'
 import { baseControlButtonLayout } from '../../utils/tailwindTemplates'
 
